33669fcf201a756c5784001b48ca2ebba21e3ae8
Thomas De Lange <thomas.m.delange@gmail.com>
2018-11-29 20:26:11 +0100

    Muber update

--- Muber/test/testHelper.js
+++ Muber/test/testHelper.js
@@ -1,16 +1,9 @@
 const mongoose = require('mongoose')
 
-before((done) => {
+before.skip('Connect to test database', (done) => {
   mongoose.connect('mongodb://localhost/muberTest' ,{ useNewUrlParser: true })
   mongoose.connection
     .once('open', () => {console.log("Database is running in test env"); done()})
     .on('error', (error) => {console.warn("Warning", error); done(error)})
 
 })
-
-beforeEach((done) => {
-  const { drivers } = mongoose.connection.collections;
-  drivers.drop()
-    .then(() => done())
-    .catch(() => done());
-});
